517e47e8f05a42d4e27665d4705810196c8611ec
e2662020 <63095313+e2662020@users.noreply.github.com>
2022-08-17 16:05:13 +0800

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,7 @@
+// var isMobile = device.mobile(),
+// 	isTable = device.tablet();
+// if(isMobile || isTable){
+// }
+// else{
+// 	window.open("http://www.xiahuaos.top","_self"); //否则打开desk.html
+// }
